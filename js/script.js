984d788afe9bf9476592a996ede2f6aa4f0c0ec6
Rodion Bulakh <bulakhrodion1@icloud.com>
2023-12-14 17:53:42 +0200

    hw-2 complete

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// task1
+let admin, name;
+name = 'John';
+admin = name;
+alert(admin);
+
+// task2
+let planetName = 'Earth';
+let visitorName = 'John';
+
+// task3
+let mixedValue = "10" + 10;
+console.log(typeof mixedValue);
+
+//у консолі виводиться тип даних string, тому що при конкатенації рядка і числа, число перетворюється в рядок, тобто у цьому випадку "10"(рядок) + 10(число) = "1010"(рядок).
