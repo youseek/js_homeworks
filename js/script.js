b2f3c6281e48954e82735369f1aeaad26c1e88ea
Rodion Bulakh <bulakhrodion1@icloud.com>
2023-12-14 18:30:29 +0200

    hw-3 complete

--- js/script.js
+++ js/script.js
@@ -6,12 +6,15 @@ const operand2 = Number(prompt('Введіть друге число'));
 //Складання
 const sum = operand1 + operand2;
 console.log(`Результат: ${operand1} + ${operand2} = ${sum}`);
+
 //Віднімання
 const difference = operand1 - operand2;
 console.log(`Результат: ${operand1} - ${operand2} = ${difference}`);
+
 //Множення
 const multiplication = operand1 * operand2;
 console.log(`Результат: ${operand1} * ${operand2} = ${multiplication}`);
+
 //Ділення
 const division = operand1 / operand2;
 console.log(`Результат: ${operand1} / ${operand2} = ${division}`);
