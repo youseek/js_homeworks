225f468b3703eb8b43a19af946f903df260e7279
Rodion Bulakh <bulakhrodion1@icloud.com>
2023-12-15 14:53:02 +0200

    fixed the mistakes

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,8 @@
 'use strict';
 
 // task1
-let admin, name;
-name = 'John';
-admin = name;
+const name = 'John';
+const admin = name;
 alert(admin);
 
 // task2
@@ -11,7 +10,7 @@ const planetName = 'Earth';
 const visitorName = 'John';
 
 // task3
-let mixedValue = "10" + 10;
+const mixedValue = "10" + 10;
 console.log(typeof mixedValue);
 
 //у консолі виводиться тип даних string, тому що при конкатенації рядка і числа, число перетворюється в рядок, тобто у цьому випадку "10"(рядок) + 10(число) = "1010"(рядок).
