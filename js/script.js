d1f62e1da99559aafb5281248d517d20145b31ba
Rodion Bulakh <bulakhrodion1@icloud.com>
2023-12-15 14:47:13 +0200

    fixed the mistakes

--- js/script.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-const Username = prompt("What is your name?");
-alert("Hello, " + Username + "! How are you?");
---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+"use strict";
+
+const username = prompt("What is your name?");
+alert("Hello, " + username + "! How are you?");
