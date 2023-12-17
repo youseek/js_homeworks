ffe1e325471934e895b68777e26c1c421ea8f162
Rodion Bulakh <bulakhrodion1@icloud.com>
2023-12-17 21:16:33 +0200

    finish hw-4

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+// task 1
+const userInput1 = prompt("Введіть перший рядок:");
+const userInput2 = prompt("Введіть другий рядок:");
+const userInput3 = prompt("Введіть третій рядок:");
+
+// Конкатенація та виведення результату
+const concatenatedString = userInput1 + " " + userInput2 + " " + userInput3;
+console.log("Результат конкатенації рядків:", concatenatedString);
+
+
+// task 2
+const fiveDigitNumber = prompt("Введіть п'ятизначне число:");
+const digitsArray = fiveDigitNumber.split('').join(' ');
+console.log("Розбиття за цифрами:", digitsArray);
