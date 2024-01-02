bc2ffaefc8bbf45d79de0f75769f9e92604a55fd
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-03 00:14:24 +0200

    finish hw-15

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const operand1 = Number(prompt('Введіть перше число'));
+const operand2 = Number(prompt('Введіть друге число'));
+const operand3 = Number(prompt('Введіть третє число'));
+
+function getAverage(num1, num2, num3) {
+    const average = (num1 + num2 + num3) / 3;
+
+    if (String(average).length === 1) {
+        alert(average);
+    } else {
+        alert(average.toFixed(2));
+    }
+}
+
+getAverage(operand1, operand2, operand3);
+
+
