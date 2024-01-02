e33ba51651f2aec1b95773a114a5f61b87048372
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-02 23:16:07 +0200

    finish hw-12

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const num1 = Number(prompt("Введіть перше число:"));
+const num2 = Number(prompt("Введіть друге число:"));
+
+if (isNaN(num1) || isNaN(num2)) {
+    alert("Будь ласка, введіть числа.");
+} else {
+    alert(
+        `${num1} + ${num2} = ${num1+num2}\n` +
+        `${num1} - ${num2} = ${num1-num2}\n` +
+        `${num1} * ${num2} = ${num1*num2}\n` +
+        `${num1} / ${num2} = ${num1/num2}`
+    );
+}
