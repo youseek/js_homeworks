abdafad3a0b1da3e14c75444a1d920f4ba3eea76
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-02 23:34:22 +0200

    finish hw-13

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const operator = prompt(`Введіть оператор (+, -, *, /) або (add, sub, mult, div)`);
+const firstNumber = Number(prompt(`Введіть перше число`));
+const secondNumber = Number(prompt(`Введіть друге число`));
+
+if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    alert(`Одне з введених значень не є числом`);
+} else if (operator === '+' || operator === 'add') {
+    alert(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+} else if (operator === '-' || operator === 'sub') {
+    alert(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+} else if (operator === '*' || operator === 'mult') {
+    alert(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+} else if (operator === '/' || operator === 'div') {
+    alert(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+} else {
+    alert(`Введіть коректний оператор`);
+}
