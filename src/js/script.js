b2259dc9ec6d16220f7412b619f49dea388bd5e1
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-10 00:40:32 +0200

    finish hw-24

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+let userInput = prompt('Введіть число:');
+let number = parseInt(userInput);
+
+if (isNaN(number) || number < 1) {
+    console.log('NaN');
+} else {
+    let minDivisor = 2;
+
+    while (minDivisor <= Math.sqrt(number)) {
+        if (number % minDivisor === 0) {
+            console.log(`Найменший дільник числа ${number}: ${minDivisor}`);
+            break;
+        }
+        minDivisor++;
+    }
+
+    if (minDivisor > Math.sqrt(number)) {
+        console.log(`Число ${number} є простим, немає дільників, крім 1 та самого себе.`);
+    }
+}
+
