adcda3d4ef6d069f5db4d1135dacec89b1cda55d
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-09 19:17:33 +0200

    finish hw-17

---
+++ Readme.md
@@ -0,0 +1,14 @@
+Переписати код нижче з використанням конструкції switch…case
+
+`let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+if(numOrStr === null) {
+console.log('ви скасували')
+} else if( numOrStr.trim() === '' ) {
+console.log('Empty String');
+} else if ( isNaN( +numOrStr ) ) {
+console.log(' number is Ba_NaN')
+} else {
+console.log('OK!')
+}`
--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+let numOrStr = prompt('input number or string');
+    console.log(numOrStr)
+
+switch (numOrStr) {
+    case null:
+        console.log('ви скасували')
+        break;
+    case '':
+        console.log('Empty String');
+        break;
+    case isNaN(+numOrStr):
+        console.log(' number is Ba_NaN')
+        break;
+    default:
+        console.log('OK!')
+        break;
+}
