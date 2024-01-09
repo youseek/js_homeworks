8e49c06a83dae50c178f0debff75dc2334297100
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-09 20:01:55 +0200

    finish hw-20

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+let userAgeInput = prompt('Введіть свій вік:');
+let userAge = parseInt(userAgeInput);
+
+if (!isNaN(userAge)) {
+    if (userAge > 10 && userAge < 20) {
+        alert(`Тобі ${userAge} років`);
+    } else {
+        switch (userAge) {
+            case 1:
+                alert(`Тобі ${userAge} рік`);
+                break;
+            case 2:
+            case 3:
+            case 4:
+                alert(`Тобі ${userAge} роки`);
+                break;
+            default:
+                alert(`Тобі ${userAge} років`);
+        }
+    }
+} else {
+    alert('Введено не число.');
+}
