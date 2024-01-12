c0e6ef8133ffbd04ac17472b58ca0c948f3746b5
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-12 18:02:01 +0200

    finish hw-23

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+console.log(sum);
+
+// 2
+const arr2 = [1, 2, 3, 4, 5];
+let sum2 = 0;
+
+for (let i = 0; i < arr2.length; i++) {
+  sum2 += arr2[i] ** 2; // Math.pow(arr2[i], 2)
+}
+console.log(sum2);
