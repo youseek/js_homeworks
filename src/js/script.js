f2175d31435426377d4bffacdab1280d91b0df83
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-13 22:44:06 +0200

    finish hw-26

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+function generateKey (length, characters) {
+    let result = '';
+    const charactersLength = characters.length;
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charactersLength);
+        result += characters.charAt(randomIndex);
+    }
+    return result;
+}
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key);
