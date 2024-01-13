784c026889a9d26f5f9ab542ccb04e4c67ef5dca
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-14 00:08:04 +0200

    finish hw-25

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,10 @@
 
 let sumOfEvenNumbers = 0;
 
-for (let i = 0; i <= 20; i++) {
-    if (i % 2 === 0) continue;
+outerLoop: for (let i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+        continue outerLoop;
+    }
     sumOfEvenNumbers += i;
 }
 console.log(sumOfEvenNumbers);
