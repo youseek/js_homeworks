020bf48e89ba0afe4b793eb91222fd24c86ca64e
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-14 00:48:48 +0200

    finish hw-27

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
-'use strict';
+"use strict";
+
+const number = +prompt("Введіть число");
+const power = +prompt("Введіть ступінь");
+
+function pow(number, power = 1) {
+  if (isNaN(number) || isNaN(power)) {
+    return "Помилка введення даних";
+  }
+
+  return number ** power;
+}
+
+alert(pow(number, power));
