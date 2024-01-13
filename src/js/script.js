2984c8beee4ae258f04c225b0c63f1e1c51409c2
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-14 01:57:51 +0200

    finish hw-28

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,27 @@
-'use strict';
+"use strict";
+
+function padString(str, targetLength, padChar = "*", padRight = true) {
+  if (!str) {
+    return "рядок не задано";
+  }
+  if (!targetLength) {
+    return "довжина не задана";
+  }
+  if (typeof str !== "string") {
+    return "рядок не є рядком";
+  }
+  if (typeof targetLength !== "number") {
+    return "довжина не є числом";
+  }
+  if (targetLength < str.length) {
+    return str.substring(0, targetLength);
+  }
+  if (padRight) {
+    return str.padEnd(targetLength, padChar);
+  } else {
+    return str.padStart(targetLength, padChar);
+  }
+}
+console.log(padString("привіт", 8));
+console.log(padString("привіт", 7, "*", false));
+console.log(padString("привіт", 2));
