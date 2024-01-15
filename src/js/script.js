c89e65c356f9162d22162725a43acc18968b06d4
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-15 20:18:27 +0200

    finish hw-29

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
-'use strict';
+"use strict";
+
+const arr = [1, 2, 3, -1, -2, -3];
+function filterArrToPositive(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("Argument is not an array");
+  }
+  if (arr.length === 0) {
+    throw new Error("Array is empty");
+  }
+  const resultArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      resultArr.push(arr[i]);
+    }
+  }
+  console.log(resultArr);
+  return resultArr.length === 0 ? null : resultArr;
+}
+filterArrToPositive(arr);
