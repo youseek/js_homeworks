e057005e5404603c00f5bf2a493c32dd99463a44
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-19 20:19:27 +0200

    finish hw-31

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
-'use strict';
+"use strict";
+
+let generatedNumbers = [];
+
+function getRandomUniqueNumber() {
+  let randomNumber = Math.floor(Math.random() * 100);
+  while (generatedNumbers.includes(randomNumber)) {
+    randomNumber = Math.floor(Math.random() * 100);
+  }
+  generatedNumbers.push(randomNumber);
+  if (generatedNumbers.length === 100) {
+    generatedNumbers = [];
+  }
+  return randomNumber;
+}
+for (let i = 0; i < 100; i++) {
+  console.log(getRandomUniqueNumber());
+}
