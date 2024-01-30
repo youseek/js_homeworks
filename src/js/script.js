b62711cf88aa4d27453a9ad8bcca91602fdc984e
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-01-30 23:14:52 +0200

    finish hw-34

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-'use strict';
+"use strict";
+
+function greet() {
+  console.log(`Привіт, ${this.name}!`);
+}
+function createUser(name, age) {
+  const user = {
+    name: name,
+    age: age,
+  };
+  user.greet = greet;
+  return user;
+}
+const user1 = createUser("Вова", 25);
+const user2 = createUser("Ольга", 30);
+user1.greet(); // Виведе: Привіт, Іван!
+user2.greet(); // Виведе: Привіт, Марія!
