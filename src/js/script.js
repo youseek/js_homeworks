505ec0bcf5af13a4e78af2d14f106c79de3031fd
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-02-15 20:19:36 +0200

    finish hw-37

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict'
+
+const parseUrl = function (url) {
+  const parser = new URL(url)
+
+  return {
+    protocol: parser.protocol,
+    host: parser.hostname,
+    port: parser.port,
+    path: parser.pathname,
+    query: parser.search,
+    hash: parser.hash,
+    origin: parser.origin
+  }
+}
+
+console.log(parseUrl('https://example.com:8080/path/name?query=123#hash'))
