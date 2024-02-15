df3bf44fe39c32cf9d09a914cbb45f5d7c784f0e
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-02-15 22:24:57 +0200

    finish hw-40

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const myReduce = (arr, callback, initialValue) => {
+  let accumulator = initialValue === undefined ? 0 : initialValue
+  for (let i = 0; i < arr.length; i++) {
+    accumulator = callback(accumulator, arr[i])
+  }
+  return accumulator
+}
+
+const arr = [1, 2, 3, 4, 5]
+const sum = myReduce(arr, (acc, value) => acc + value)
+console.log(sum) // 15
