f7f9852f43756c8e9829429ea249b5e34384411a
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-02-18 12:07:09 +0200

    finish hw-46

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const user = {
+  name: 'John',
+  age: 30,
+  gender: 'male'
+}
+
+const printName = function (prefix) {
+  console.log(prefix + ' ' + this.name)
+}
+
+const myApply = function (func, ctx, args) {
+  ctx.printName = func
+  ctx.printName(...args)
+  delete ctx.printName
+}
+
+myApply(printName, user, ['Mr.'])
