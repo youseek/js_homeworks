dc992164c6df6a6f1f4e8f8fe00019c9e292e793
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-02-18 14:09:39 +0200

    finish hw-45

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const ladder = {
+  step: 0,
+  up () {
+    this.step++
+    return this
+  },
+  down () {
+    this.step--
+    return this
+  },
+  showStep () {
+    alert(this.step)
+    return this
+  }
+}
+
+ladder.up().up().down().showStep()
