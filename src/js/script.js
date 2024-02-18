9353f6b9bb1a87072151bf8e63908b80df8f7339
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-02-18 12:11:48 +0200

    finish hw-47

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 'use strict'
+
+const myBind = function (func, ctx, ...args) {
+  return function (...bindArgs) {
+    return func.apply(ctx, [...args, ...bindArgs])
+  }
+}
+
+const user = {
+  name: 'John',
+  age: 30,
+  gender: 'male'
+}
+
+const printInfo = function (prefix) {
+  console.log(prefix + ' ' + this.name + ', age ' + this.age)
+}
+
+const boundFunction = myBind(printInfo, user, 'Mr.')
+boundFunction()
