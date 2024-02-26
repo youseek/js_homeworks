eb886982688cf6fe0cf370d923b5f3932df17746
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-02-26 23:08:30 +0200

    finish hw-51

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
 'use strict'
+const parseJSON = (jsonString) => {
+  try {
+    return JSON.parse(jsonString)
+  } catch (error) {
+    return 'Invalid JSON string'
+  }
+}
+console.log(parseJSON('{"name": "John", "age": 30}'))
+console.log(parseJSON('{"name": "John", "age":'))
