106f9325e59602c6ae8dea558fc2a2fddb3d2d00
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-02-26 22:45:55 +0200

    finish hw-48

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
 'use strict'
+const sum = (a, b) => a + b
+const stringLength = str => str.length
+const incrementArray = arr => arr.map(num => num + 1)
+
+console.log(sum(3, 5)) // Виведе: 8
+console.log(stringLength('Hello')) // Виведе: 5
+console.log(incrementArray([1, 2, 3, 4, 5])) // Виведе: [2, 3, 4, 5, 6]
