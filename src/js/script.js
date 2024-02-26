95c15a0006da9a41e09cbbde1be97794770d4f64
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-02-26 23:03:16 +0200

    finish hw-50

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
 'use strict'
+
+const products = {
+  apple: 10,
+  banana: 20,
+  orange: 15,
+  kiwi: 12
+}
+const decreasePrices = (products) => {
+  const updatedPrices = {}
+  for (const key in products) {
+    updatedPrices[key] = products[key] * 0.9
+  }
+  return updatedPrices
+}
+console.log(decreasePrices(products))
+
+const students = [
+  { name: 'John', grade: 85 },
+  { name: 'Alice', grade: 92 },
+  { name: 'Bob', grade: 78 },
+  { name: 'Emma', grade: 95 }
+]
+const topStudents = (students) => students.filter((student) => student.grade > 90)
+console.log(topStudents(students))
