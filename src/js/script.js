97aa3549af43d6d497a8765ecc40f84dadf81aab
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-02-26 22:56:42 +0200

    finish hw-49

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 'use strict'
+const stringsArray = ['apple', 'banana', 'orange', 'kiwi', 'grapefruit', 'watermelon', 'pineapple', 'mango', 'papaya', 'pear']
+const filteredArray = stringsArray.filter((str) => str.length > 5)
+console.log(filteredArray)
+
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const sumOfEvenNumbers = numbersArray.filter((num) => num % 2 === 0).reduce((acc, cur) => acc + cur, 0)
+console.log(sumOfEvenNumbers)
+
+const average = numbersArray.reduce((acc, cur) => acc + cur, 0) / numbersArray.length
+console.log(average)
