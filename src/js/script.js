22fbdc18f3a7ca66280d446e7b6daf81225b0113
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-04-06 22:53:46 +0300

    finish hw-60

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const form = document.querySelector('form')
+const button = document.querySelector('button')
+
+button.addEventListener('click', (event) => {
+  event.preventDefault()
+  const formStatus = document.querySelector('p')
+  const formData = new FormData(form)
+  if (formData.get('name') === '' || formData.get('email') === '' || formData.get('password') === '') {
+    formStatus.textContent = 'Please fill in all fields'
+    return
+  }
+  formStatus.textContent = 'Form submitted successfully!'
+})
