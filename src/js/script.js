ad3f5a57ab3c7c5898b5c858e4afe9bf583c8318
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-04-06 18:20:15 +0300

    finish hw-54

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const body = document.querySelector('body')
+const generateList = function (arr) {
+  if (!Array.isArray(arr) || arr.length === 0) {
+    return null
+  }
+  const ul = document.createElement('ul')
+  arr.forEach(item => {
+    const li = document.createElement('li')
+
+    if (Array.isArray(item)) {
+      li.appendChild(generateList(item))
+    }
+
+    li.textContent = item
+    ul.appendChild(li)
+  })
+  return ul
+}
+
+const testArr = [1, 2, [1.1, 1.2, 1.3], 3]
+
+body.appendChild(generateList(testArr))
