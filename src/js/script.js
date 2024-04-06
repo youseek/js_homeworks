341466b5a64a856947d5c4adfe6143b7b6e68677
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-04-07 00:56:14 +0300

    finish hw-55

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,20 @@
 'use strict'
+const createTable = function (rows, cols) {
+  const table = document.createElement('table')
+  for (let i = 0; i < rows; i++) {
+    const tr = document.createElement('tr')
+    for (let j = 1; j < cols + 1; j++) {
+      const td = document.createElement('td')
+      if (i === 0) {
+        td.textContent = `${j}`
+      } else {
+        td.textContent = `${j + i * 10}`
+      }
+      tr.appendChild(td)
+    }
+    table.appendChild(tr)
+  }
+  return table
+}
+const table = createTable(10, 10)
+document.body.appendChild(table)
