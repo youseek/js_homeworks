292b7bca632da601cc4d745c72e849f5d1b33010
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-04-06 19:42:17 +0300

    finish hw-59

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const notes = document.getElementById('notes')
+const button = document.getElementById('save')
+
+const createNote = () => {
+  const textInput = document.getElementById('textInput')
+  if (textInput.value === '' || textInput.value.trim() === '') {
+    alert('Please enter a note')
+  }
+  const note = document.createElement('p')
+  note.innerHTML = textInput.value
+  notes.prepend(note)
+  textInput.value = ''
+}
+button.addEventListener('click', createNote)
