d38ef14a7237606987a19676690e6b0b4c8c6568
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-04-06 18:41:47 +0300

    finish hw-58

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
 'use strict'
+const checkNumber = function () {
+  const number = document.getElementById('numberInput').value
+  if (number % 2 === 0) {
+    alert('Парне число')
+  } else {
+    alert('Непарне число')
+  }
+}
