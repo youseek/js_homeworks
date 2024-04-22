b01d30e4104bdef9a08de4c240b3905ef4c17a18
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-04-22 13:09:33 +0300

    finish hw-62

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict'
+
+document.getElementById('form').addEventListener('submit', function (event) {
+  event.preventDefault()
+
+  const formData = new FormData(this)
+  const data = {}
+  formData.forEach(function (value, key) {
+    data[key] = value
+  })
+  localStorage.setItem('formData', JSON.stringify(data))
+  this.reset()
+  window.location.href = 'data.html'
+})
