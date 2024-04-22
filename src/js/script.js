738d3f4794efa3e1467d0c3d93e931ced0efc940
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-04-22 16:09:23 +0300

    finish hw-70

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
 'use strict'
+
+const customIterator = function (arr) {
+  let index = 0
+
+  return {
+    next: function () {
+      return index < arr.length
+        ? { value: arr[index++], done: false }
+        : { done: true }
+    }
+  }
+}
+
+const arr = [1, 2, 3, 4, 5]
+arr[Symbol.iterator] = function () {
+  return customIterator(this)
+}
+
+const iterator = arr[Symbol.iterator]()
+
+let result = iterator.next()
+while (!result.done) {
+  console.log(result.value)
+  result = iterator.next()
+}
