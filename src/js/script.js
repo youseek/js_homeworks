974ffb3405a5d9e5cd818bbc4d928d9a2a47bc42
Rodion Bulakh <bulakhrodion1@icloud.com>
2024-04-23 15:12:07 +0300

    finish hw-81

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,48 @@
 'use strict'
+
+const user = {}
+
+// Практичне завдання
+
+Object.defineProperty(user, 'name', {
+  value: 'John',
+  writable: false
+})
+
+Object.defineProperty(user, 'age', {
+  value: 30,
+  enumerable: false
+})
+
+// Розширене завдання
+
+Object.defineProperty(user, 'fullName', {
+  get: function () {
+    return `${this._name} ${this.surname || ''}`
+  },
+  set: function (value) {
+    const parts = value.split(' ')
+    this._name = parts[0]
+    this.surname = parts[1] || ''
+  }
+})
+
+user.fullName = 'John Doe'
+
+console.log(user._name)
+console.log(user.surname)
+
+// Додаткове завдання
+
+const frozenObj = {
+  fullName: 'Alice'
+}
+
+frozenObj.name = 'John'
+Object.freeze(frozenObj)
+
+try {
+  Object.defineProperty(frozenObj, 'name', { writable: true })
+} catch (error) {
+  console.log(error.message)
+}
